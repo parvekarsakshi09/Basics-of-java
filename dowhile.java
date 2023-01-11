6820113303a2c672bb365682ae23f4e8fe33f829
Sakshi Yashvant Parvekar <112756881+parvekarsakshi09@users.noreply.github.com>
2023-01-11 23:35:35 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,29 @@
+public class dowhile {
+    public static void main(String[] args) {
+        //this first loop enters only once due to false condition
+        int i = 890;
+        do {
+            System.out.println(i);
+            i++;
+
+
+        } while (i < 50);
+
+        int ia = 8;
+        do {
+            System.out.println(ia);
+            ia++;
+
+
+        } while (ia < 50);
+
+
+        int ik = 3;
+        do {
+           // System.out.println("infine loop");
+            ik++;
+        }
+        while (true);
+
+    }
+}
