6afb526152037fc2ddb71d1b0b9b902ab8128dce
Sakshi Yashvant Parvekar <112756881+parvekarsakshi09@users.noreply.github.com>
2023-01-11 23:35:56 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,19 @@
+public class forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+            int n = 5;
+            for (int l = 0; l <= n; l++) {
+                System.out.println(2 * l);
+            }
+                for (int j = 100; j != 0; j--) {
+                    System.out.println(j);
+                }
+                    for (int k = 100; k > 0; k--) {
+                        System.out.println(k);
+
+
+        }
+    }
+}
