fceb3e88f8dcbc2ce12f584071af38d578ee7b62
Sakshi Yashvant Parvekar <112756881+parvekarsakshi09@users.noreply.github.com>
2023-01-11 22:46:23 +0530

    Add files via upload

---
+++ relationalandlogicaloperators.java
@@ -0,0 +1,25 @@
+package com.company;
+public class relationalandlogicaloperators {
+    public static void main(String[] args) {
+        boolean a=false;
+        boolean b=false;
+        if(a&&b){
+            System.out.println("y");
+        }
+        else{
+            System.out.println("n");
+        }
+        if(a||b){
+            System.out.println("y");
+        }
+        else{
+            System.out.println("n");
+        }
+
+     System.out.println(!a);
+        System.out.println(!b);
+
+
+    }
+}
+
