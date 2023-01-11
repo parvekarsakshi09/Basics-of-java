de86ecb52500f8da13410a9360f8a6a1f71d6018
Sakshi Yashvant Parvekar <112756881+parvekarsakshi09@users.noreply.github.com>
2023-01-11 17:54:59 +0530

    Add files via upload

---
+++ datatypesandvariables.java
@@ -0,0 +1,23 @@
+
+public class datatypesandvariables {
+    public static void main(String[] args) {
+        int a = 23;
+        float b = 478.42f;
+        double c = 98763174831.444;
+        long d = 804507640761L;
+        short e = 7;
+        boolean f = true;
+        byte g = 87;
+        char h = 'y';
+        String str = "sakshi";
+        System.out.println(str);
+        System.out.println(f);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
