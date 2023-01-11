0f1a8117e09498b2173427a1c44da649c79cccb0
Sakshi Yashvant Parvekar <112756881+parvekarsakshi09@users.noreply.github.com>
2023-01-11 22:43:33 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ifelse {
+    public static void main(String[] args) {
+
+       int age=28;
+        boolean cond=(age>18);
+        if(cond){
+            System.out.println("you can vote");
+        }
+        else{
+            System.out.println("no not");
+        }
+    }
+}
