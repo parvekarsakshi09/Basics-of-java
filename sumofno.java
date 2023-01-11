697e8da9cd30d58874d66956f1f634f91d874c1d
Sakshi Yashvant Parvekar <112756881+parvekarsakshi09@users.noreply.github.com>
2023-01-11 19:23:11 +0530

    Add files via upload

---
+++ sumofno.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class sumofno {
+    public static void main(String[] args) {
+        System.out.println("input user");
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("nu1");
+        float a=sc.nextFloat();
+        System.out.println("num2");
+         float b=sc.nextFloat();
+         float c=a+b;
+        System.out.println(c);
+        String str=sc.nextLine();
+        System.out.println(str);
+        boolean b1=sc.hasNextInt();
+        System.out.println(b1);
+
+
+    }
+}
