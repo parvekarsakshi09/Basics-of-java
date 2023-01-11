32f47afd4e4921032356f044ae16ceb652388727
Sakshi Yashvant Parvekar <112756881+parvekarsakshi09@users.noreply.github.com>
2023-01-11 23:35:12 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,15 @@
+public class whileloop {
+    public static void main(String[]args){
+
+        int i=1;
+        while(i<5){
+            System.out.println(i);
+            i++;
+        }
+        int iw=100;
+        while(true){
+           // System.out.println("iam infinite loop");
+            iw++;
+        }
+    }
+}
