092e928802c8d62d19808f0736b6d992e62d184d
Sakshi Yashvant Parvekar <112756881+parvekarsakshi09@users.noreply.github.com>
2023-01-11 19:32:21 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class strings {
+    public static void main(String[] args)
+    {
+        String str="sakshi";
+        System.out.println(str);
+        String st=new String("haus");
+        System.out.println(st);
+        Scanner s=new Scanner(System.in);
+        String so=s.next();
+        System.out.println(so);
+
+     int a=73;
+     float b=973.387474f;
+        System.out.printf("value of a is %5d anf b is %7.2f",a,b);
+
+
+
+    }
+}
